4f308d3eeaa07677bb3dcc0493372deb5de9b43a
codeunlu <codeunlu@gmail.com>
2021-08-11 23:48:34 +0300

    UI-Feat: LoginPage designed and login operation added.

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import {Card,Container,Title,InputBox,LoginButton} from './styles'
-import {FormGroup,Form} from 'reactstrap'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Card, Container, Title, InputBox, LoginButton } from "./styles";
+import { FormGroup, Form } from "reactstrap";
+import firebase from "../../firebase/firebase";
 
 function LoginPage() {
-  return (
+  let history = useHistory();
 
+  const [user, setUser] = useState({ userName: "" });
+  const userRef = firebase.database().ref("users/");
+
+  const changeHandler = (event) => {
+    setUser({ ...user, userName: event.target.value });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (user.userName !== "") {
+      userRef
+        .orderByChild("userName")
+        .equalTo(user.userName)
+        .once("value", (snap) => {
+          if (snap.exists()) {
+            localStorage.setItem("userName",user.userName)
+            history.push("/roomlist");
+          }else{
+            const newUser = userRef.push();
+            newUser.set(user);
+            localStorage.setItem("userName",user.userName)
+            history.push("/roomlist");
+          } 
+        });
+    }
+  };
+
+  return (
     <Container>
       <Card>
         <Title>HEY DUDE !</Title>
-        <Form>
+        <Form onSubmit={submitHandler}>
           <FormGroup>
-              <InputBox type="text" name="nickname" id="nickname" placeholder="Enter Your Nickname"  />
+            <InputBox
+              type="text"
+              name="userName"
+              id="userName"
+              placeholder="Enter Your Nickname"
+              value={user.userName}
+              onChange={changeHandler}
+            />
           </FormGroup>
-          <LoginButton  type="submit">
-              Login
-          </LoginButton>
+          <LoginButton type="submit">Login</LoginButton>
         </Form>
       </Card>
     </Container>
-  )
+  );
 }
 
 export default LoginPage;
