b427ee490094758271d29cf69018737505cab3e5
Ahmet Kelek <ahmet.kelek10@gmail.com>
2021-08-11 18:17:39 +0300

    Feat: React router dom installed and App.js editted.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import LoginPage from "./components/LoginPage/LoginPage";
+import ChatRoom from "./components/ChatRoom/ChatRoom";
+import AddRoom from "./components/AddRoom/AddRoom";
+import RoomList from "./components/RoomList/RoomList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/addroom">
+          <AddRoom />
+        </Route>
+        <Route path="/chatroom/:room">
+          <ChatRoom />
+        </Route>
+        <Route path="/roomlist">
+          <RoomList />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
