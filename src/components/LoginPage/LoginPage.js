a133989dbb2a7891804c13c86964418131e0108b
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-08-11 19:17:16 +0300

    Feat: Firebase and Styled Component installed.

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function LoginPage() {
   return (
     <div>
-      
+      LoginPage
     </div>
   )
 }
--- src/components/RoomList/RoomList.js
+++ src/components/RoomList/RoomList.js
@@ -3,7 +3,7 @@ import React from 'react';
 function RoomList() {
   return (
     <div>
-      
+      RoomList
     </div>
   )
 }
