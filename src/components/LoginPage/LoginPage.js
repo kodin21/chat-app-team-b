f5f447d08eb9270f0017f5256176b3a80917ebd1
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-08-11 20:57:29 +0300

    UI: Login Page designed. Co-authored-by: Mustafa ÜNLÜ <codeunlu@users.noreply.github.com>

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {Card,Container,Title,InputBox,LoginButton} from './styles'
+import {FormGroup,Form} from 'reactstrap'
 
 function LoginPage() {
   return (
-    <div>
-      LoginPage
-    </div>
+
+    <Container>
+      <Card>
+        <Title>HEY DUDE !</Title>
+        <Form>
+          <FormGroup>
+              <InputBox type="text" name="nickname" id="nickname" placeholder="Enter Your Nickname"  />
+          </FormGroup>
+          <LoginButton  type="submit">
+              Login
+          </LoginButton>
+        </Form>
+      </Card>
+    </Container>
   )
 }
 
