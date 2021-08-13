c104b13d8ff2511be45a1aec3ef80bee6b75fc3e
Mehmet Alperen <me.alperen@gmail.com>
2021-08-13 21:32:38 +0300

    Ref: roomTitleUrl refactored.

--- src/components/RoomList/RoomList.js
+++ src/components/RoomList/RoomList.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Card, Container, RoomButton, Title, RoomContainer } from "./styles";
-import firebase from "../../firebase/firebase";
-import AddRoom from "../AddRoom/AddRoom";
+import React, { useEffect, useState } from 'react';
+import { Card, Container, RoomButton, Title, RoomContainer } from './styles';
+import firebase from '../../firebase/firebase';
+import AddRoom from '../AddRoom/AddRoom';
 
 function RoomList() {
   const [rooms, setRooms] = useState([]);
 
-  const roomRef = firebase.database().ref("rooms/");
-  const roomUsersRef = firebase.database().ref("roomusers/");
+  const roomRef = firebase.database().ref('rooms/');
+  const roomUsersRef = firebase.database().ref('roomusers/');
 
   let roomUser = localStorage.getItem('userName');
   const newRoomUser = { roomname: '', nickname: '', status: '' };
 
   useEffect(() => {
-    roomRef.on("value", (snap) => {
+    roomRef.on('value', (snap) => {
       setRooms([snap.val()]);
     });
   }, []);
@@ -22,49 +22,58 @@ function RoomList() {
     const returnArr = [];
 
     snapshot.forEach((childSnapshot) => {
-        const item = childSnapshot.val();
-        item.key = childSnapshot.key;
-        returnArr.push(item);
+      const item = childSnapshot.val();
+      item.key = childSnapshot.key;
+      returnArr.push(item);
     });
 
     return returnArr;
-  }
+  };
 
   const click = (roomname) => {
+    roomUsersRef
+      .orderByChild('nickname')
+      .equalTo(roomUser)
+      .on('value', (resp) => {
+        let participants = [];
+        participants = snapshotToArray(resp);
+        const user = participants.find(
+          (participant) => participant.nickname === roomUser
+        );
+        if (user === undefined) {
+          newRoomUser.roomname = roomname;
+          newRoomUser.nickname = roomUser;
+          newRoomUser.status = 'online';
 
-    roomUsersRef.orderByChild('nickname').equalTo(roomUser).on('value', (resp) => {
-      let participants = [];
-      participants = snapshotToArray(resp);
-      const user = participants.find(participant => participant.nickname === roomUser);
-      if(user===undefined)
-      {
-        newRoomUser.roomname=roomname;
-        newRoomUser.nickname=roomUser;
-        newRoomUser.status="online";
-    
-        const newRoomUser =roomUsersRef.push();
-        newRoomUser.set(newRoomUser);
-      }
-    });
+          const newRoomUser = roomUsersRef.push();
+          newRoomUser.set(newRoomUser);
+        }
+      });
   };
-  
-  const roomTitleUrl = (roomName) => {
 
-    return roomName.replace(/ /g, '-').toLowerCase();
-  }
+  const roomTitleUrl = (roomName) => {
+    return roomName.replace(/ /g, '-');
+  };
 
   return (
     <Container>
       <Card>
         <Title>Rooms</Title>
         <AddRoom />
-        <RoomContainer >
-          {
-            rooms[0] && Object.values(rooms[0]).map((room, index) => (
-              <RoomButton to={{pathname: `/rooms/${roomTitleUrl(room.room)}` }} key={index} action onClick={() => { click(room.room) }} >{room.room} 
+        <RoomContainer>
+          {rooms[0] &&
+            Object.values(rooms[0]).map((room, index) => (
+              <RoomButton
+                to={{ pathname: `/rooms/${roomTitleUrl(room.room)}` }}
+                key={index}
+                action
+                onClick={() => {
+                  click(room.room);
+                }}
+              >
+                {room.room}
               </RoomButton>
-            ))
-          }
+            ))}
         </RoomContainer>
       </Card>
     </Container>
