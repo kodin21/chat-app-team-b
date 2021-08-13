16bac26ee9c40b2e9adbe164641664fd1f874ade
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-08-14 01:32:44 +0300

    Ref: Firebase.js editted.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,8 +3,8 @@ import firebase from "firebase";
 const settings = { timestampsInSnapshots: true };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2tei_RJB8CiYhm8CQqUF_7UyUdRI8wb8",
-  projectId: "beste-80688",
+  apiKey: process.env.REACT_API_KEY,
+  projectId: process.env.REACT_API_PROJECT_ID,
   databaseURL: "https://beste-80688-default-rtdb.firebaseio.com",
 };
 
