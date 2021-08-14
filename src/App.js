f5c84218dc6328e46cc2eee1e25fb6b52a4be20d
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-08-14 12:21:25 +0300

    UI: Change theme created.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,9 +9,21 @@ import LoginPage from "./components/LoginPage/LoginPage";
 import ChatRoom from "./components/ChatRoom/ChatRoom";
 import AddRoom from "./components/AddRoom/AddRoom";
 import RoomList from "./components/RoomList/RoomList";
+import {BsSun,BsMoon} from 'react-icons/bs'
+
 
 function App() {
+  const [darkMode, setDarkMode]=useState();
+  const toggleDarkMode =()=>setDarkMode(darkMode ? false :true);
+  useEffect(() => {
+    console.log(`Is in dark mode? ${darkMode}`);
+  }, [darkMode]);
   return (
+    <div className="App" data-theme={darkMode ? 'dark' :'light'}>
+      <div id ='icon'  onClick={toggleDarkMode} >
+      {darkMode ? <BsSun size='sm'/> : <BsMoon size='sm' />}
+    </div>
+
     <Router>
       <Switch>
         <Route path="/login">
@@ -31,6 +43,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
+    </div>
   );
 }
 
